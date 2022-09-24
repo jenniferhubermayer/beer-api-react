88fb95ed79b467ec38e8bdfc8be78dfefd27bfc7
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-09-24 18:06:10 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
-// import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Beers from "./Pages/Beers";
 import Details from "./Pages/Details";
 
 function App() {
-  // const [beers, setBeers] = useState([]);
-  // useEffect(() => {
-  //   fetch("https://ih-beers-api2.herokuapp.com/beers")
-  //     .then((response) => response.json())
-  //     .then((data) => setBeers(data));
-  // }, []);
-  // console.log(beers);
   return (
     <BrowserRouter>
       <div className="App">
